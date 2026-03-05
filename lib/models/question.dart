34e54a9b31a6bb29cb6bd2249b248ed54c62d526
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-03-05 07:46:19 -0500

    survey questions

--- lib/models/question.dart
+++ lib/models/question.dart
@@ -17,3 +17,36 @@ class Question {
     this.maxLines = 1,
   });
 }
+
+// Survey question definitions
+const List<Question> surveyQuestions = [
+  Question(
+    id: 'name',
+    title: 'What is your name?',
+    type: QuestionType.text,
+  ),
+  Question(
+    id: 'theme',
+    title: 'What theme do you use?',
+    type: QuestionType.multipleChoice,
+    options: ['Dark', 'Light'],
+  ),
+  Question(
+    id: 'pages',
+    title: 'What pages do you use? (Select all that apply)',
+    type: QuestionType.checkbox,
+    options: ['Off', 'Pit/Drive', 'Pit/Reverse', 'Endurance', 'Efficiency'],
+  ),
+  Question(
+    id: 'liked_features',
+    title: 'What features do you like?',
+    type: QuestionType.text,
+    maxLines: 4,
+  ),
+  Question(
+    id: 'feature_requests',
+    title: 'Are there any features you would request or improve to enhance your user experience?',
+    type: QuestionType.text,
+    maxLines: 4,
+  ),
+];

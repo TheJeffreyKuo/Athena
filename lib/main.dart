22d408ee56197e5e36f35dec5038427196556595
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-03-05 16:09:16 -0500

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 import 'screens/survey_screen.dart';
 import 'screens/responses_screen.dart';
@@ -7,6 +8,8 @@ import 'screens/responses_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // Enables offline persistence for mobile platforms
+  FirebaseFirestore.instance.settings = const Settings(persistenceEnabled: true);
   runApp(const AthenaApp());
 }
 
@@ -24,7 +27,47 @@ class AthenaApp extends StatelessWidget {
         useMaterial3: true,
         brightness: Brightness.light,
       ),
-      home: const ResponsesScreen(),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+// Bottom navigation between survey and responses
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final _screens = const [
+    SurveyScreen(),
+    ResponsesScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (i) => setState(() => _currentIndex = i),
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.assignment_outlined),
+            selectedIcon: Icon(Icons.assignment),
+            label: 'Survey',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.storage_outlined),
+            selectedIcon: Icon(Icons.storage),
+            label: 'Responses',
+          ),
+        ],
+      ),
     );
   }
 }
